1755def905eca90d8489bb6f9816b74d7beff40f
Mike Zhu <mike.zhu@strikingly.com>
2019-03-27 14:03:00 +0800

    fix: update bundle to remove error code

--- gallery-demo/bin/app.bundle.js
+++ gallery-demo/bin/app.bundle.js
@@ -44,8 +44,7 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	__webpack_require__(1);
-	(function webpackMissingModule() { throw new Error("Cannot find module \"build\""); }());
+	module.exports = __webpack_require__(1);
 
 
 /***/ },
